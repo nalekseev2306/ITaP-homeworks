e7f5fc1d3f2c7a5bfb5ea4b5f3eff5c3e3df61a2
nalekseev <alekseevn2306@gmail.com>
2025-04-04 13:54:34 +0400

    now postfix works for all numbers

--- Tasks2Term/#3/postfix.cpp
+++ Tasks2Term/#3/postfix.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 struct stack {
-    char inf;
+    string inf;
     stack* next;
 };
 
 // добовляем элемент в начало стека
-void push(stack*& h, char x) {
+void push(stack*& h, string x) {
     stack* r = new stack;
     r->inf = x;
     r->next = h;
@@ -17,32 +18,54 @@ void push(stack*& h, char x) {
 }
 
 // удаляем элемент и возвращаем его значение
-char pop(stack*& h) {
-    char x = h->inf;
+string pop(stack*& h) {
+    string x = h->inf;
     stack* r = h;
     h = h->next;
     delete r;
     return x;
 }
 
-int priority(char ch) {
-    if(ch == '(' || ch == ')') return 1;
-    else if(ch == '+' || ch == '-') return 2;
-    else if(ch == '*' || ch == '/') return 3;
+int priority(string ch) {
+    if(ch == "(" || ch == ")") return 1;
+    else if(ch == "+" || ch == "-") return 2;
+    else if(ch == "*" || ch == "/") return 3;
     else return 0;
 }
 
+// разбиваем выражение на состовляющие
+vector<string> getElems(string str) {
+    vector<string> elems;
+    while(str.find_first_of("()+-*/") != -1) {
+        string buf = str.substr(0, str.find_first_of("()+-*/") + 1);
+        if(buf.size() == 1) {
+            elems.push_back(buf);
+            str.erase(str.begin(), str.begin() + 1);
+        }
+        else {
+            buf = str.substr(0, str.find_first_of("()+-*/"));
+            elems.push_back(buf);
+            str.erase(str.begin(), str.begin() + str.find_first_of("()+-*/"));
+        }
+    }
+    elems.push_back(str);
+    return elems;
+}
+
+// переводим из инфиксной записи в постфиксную
 string result(string str) {
     string res = "";
+    vector<string> elems = getElems(str);
     stack* h = NULL;
     int i = 0;
-    while(i != str.size())  {
-        int ch = str[i];
+    while(i != elems.size())  {
+        string ch = elems[i];
+        if(ch.size() > 1) ch += ".";
         if(priority(ch) == 0) res += ch;
-        else if(ch == '(') push(h, ch);
+        else if(ch == "(") push(h, ch);
         else if(!h && (priority(ch) == 2 || priority(ch) == 3)) push(h, ch);
-        else if(ch == ')') {
-            while(h->inf != '(') {
+        else if(ch == ")") {
+            while(h->inf != "(") {
                 res += pop(h);
             }
             pop(h);
@@ -67,7 +90,7 @@ int main() {
     getline(cin, str);
     cout << "Result: " << result(str) << endl;
 
-    // system("pause");
+    system("pause");
     return 0;
 
 }
